b4fd52d42a96eea087c4563ecb7156c094fb7d36
Katyani Reddy <katyanireddy12@gmail.com>
2026-06-08 12:20:55 +0530

    homepage delta

--- delta/script.js
+++ delta/script.js
@@ -27,3 +27,113 @@ function openAdmin() {
 
   }
 }
+const results = [
+
+{
+  image:"../images/topper1.jpeg",
+  name:"Arushi Sinha",
+  score:"96.4%"
+},
+
+{
+  image:"../images/ishaan.jpeg",
+  name:"Ishaan Qayanat",
+  score:"419 (IIT JEE)"
+},
+
+{
+  
+  image:"../images/pallavi.jpeg",
+  name:"Pallavi Kri",
+  score:"455 (91%)"
+},
+
+{
+  image:"../images/shatanshi.jpeg",
+  name:"Shatanshi Kri",
+  score:"435"
+}
+
+];
+
+let current = 0;
+
+function updateCard(){
+
+ document.getElementById("resultImage").src =
+ results[current].image;
+
+ document.getElementById("resultName").textContent =
+ results[current].name;
+
+ document.getElementById("resultScore").textContent =
+ results[current].score;
+
+}
+
+function nextResult(){
+
+ current++;
+
+ if(current >= results.length){
+   current = 0;
+ }
+
+ updateCard();
+
+}
+
+function prevResult(){
+
+ current--;
+
+ if(current < 0){
+   current = results.length - 1;
+ }
+
+ updateCard();
+
+}
+document
+.getElementById("enquiryForm")
+?.addEventListener("submit", submitEnquiry);
+
+async function submitEnquiry(event) {
+
+    event.preventDefault();
+
+    const name =
+        document.getElementById("parentName").value;
+
+    const email =
+        document.getElementById("email").value;
+
+    const phone =
+        document.getElementById("phone").value;
+
+    const message =
+        document.getElementById("message").value;
+
+    const { error } = await supabaseClient
+        .from("coaching_enquiries")
+        .insert([
+            {
+                name: name,
+                email: email,
+                phone: phone,
+                message: message
+            }
+        ]);
+
+    if (error) {
+        console.error(error);
+        alert("Enquiry Failed ❌");
+        return;
+    }
+
+    alert("Enquiry Sent Successfully ✅");
+
+    document
+        .getElementById("enquiryForm")
+        .reset();
+}
